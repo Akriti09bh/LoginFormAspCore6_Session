820f9bc1b093dc9f5186dd12a821d794641cf652
Ezee1234 <akriti09bhardwaj@gmail.com>
2024-01-24 16:21:45 +0530

    Add project files.

---
+++ Models/UserTbl.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace LoginFormAspCore6_Session.Models
+{
+    public partial class UserTbl
+    {
+        [Required]
+        public int? Id { get; set; }
+
+        [Required]
+        public string Name { get; set; } = null!;
+
+        [Required]
+        public string Gender { get; set; } = null!;
+
+        [Required]
+        public decimal? Age { get; set; }
+
+        [Required]
+        public string Email { get; set; } = null!;
+
+        [DataType(DataType.Password)]
+        [Required]
+        public string Password { get; set; } = null!;
+    }
+}
---
+++ Models/db1Context.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+namespace LoginFormAspCore6_Session.Models
+{
+    public partial class db1Context : DbContext
+    {
+        public db1Context()
+        {
+        }
+
+        public db1Context(DbContextOptions<db1Context> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<UserTbl> UserTbls { get; set; } = null!;
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<UserTbl>(entity =>
+            {
+                entity.ToTable("user_tbl");
+
+                entity.Property(e => e.Id)
+                    .ValueGeneratedNever()
+                    .HasColumnName("id");
+
+                entity.Property(e => e.Age)
+                    .HasColumnType("numeric(18, 0)")
+                    .HasColumnName("age");
+
+                entity.Property(e => e.Email)
+                    .HasMaxLength(50)
+                    .IsUnicode(false)
+                    .HasColumnName("email");
+
+                entity.Property(e => e.Gender)
+                    .HasMaxLength(10)
+                    .IsUnicode(false)
+                    .HasColumnName("gender");
+
+                entity.Property(e => e.Name)
+                    .HasMaxLength(50)
+                    .IsUnicode(false)
+                    .HasColumnName("name");
+
+                entity.Property(e => e.Password)
+                    .HasMaxLength(10)
+                    .IsUnicode(false)
+                    .HasColumnName("password");
+            });
+
+            OnModelCreatingPartial(modelBuilder);
+        }
+
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+    }
+}
